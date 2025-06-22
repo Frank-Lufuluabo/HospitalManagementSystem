6bf73a1f8e022f3123d3771f2d4e0872c6a984b6
frankkalengay@gmail.com <frankkalengay@gmail.com>
2025-06-22 23:59:48 +0200

    Finish addNewSystemCode page features

--- HospitalManagementSystem/Program.cs
+++ HospitalManagementSystem/Program.cs
@@ -42,6 +42,7 @@ builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.Requ
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
 builder.Services.AddScoped<ISystemCodeRepository, SystemCodeRepository>();
+builder.Services.AddScoped<ISystemCodeDetailsRepository, SystemCodeDetailsRepository>();
 
 builder.Services.AddMudServices();
 
